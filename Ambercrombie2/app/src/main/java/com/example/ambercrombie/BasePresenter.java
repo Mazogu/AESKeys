b52f876058ca7687767e72f2b1adff4147ac8c3b
Mazogu <michael_doom@hotmail.com>
2019-06-21 00:14:14 -0600

    Reorganized Dagger configurations, changed ui design.

--- Ambercrombie2/app/src/main/java/com/example/ambercrombie/BasePresenter.java
+++
@@ -1,5 +0,0 @@
-package com.example.ambercrombie;
-
-public interface BasePresenter<V extends BaseView> {
-    void attachView(V view);
-}
--- Ambercrombie2/app/src/main/java/com/example/ambercrombie/BaseView.java
+++
@@ -1,5 +0,0 @@
-package com.example.ambercrombie;
-
-public interface BaseView {
-    void showError(String s);
-}
